a9d98c5a9119294c84070696206e442dcd2a8b2e
yizigezi <54109315+yizigezi@users.noreply.github.com>
2024-06-17 17:06:51 +0800

    a

--- QuickProject.py
+++ QuickProject.py
@@ -1,4 +1,9 @@
 from Includes.org.cora.CoraOS.Essentials.data.json.DataStorage import DStorage
+from Includes.org.cora.CoraOS.Essentials.fs.Type.basictype import Folder, FolderNotFound, TypeERROR
 import os
 CurrentWorkDir = os.getcwd()
 print(CurrentWorkDir)
+try:
+    Folder(".quickproject")
+except FolderNotFound or TypeERROR:
+    
